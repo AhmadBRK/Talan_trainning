efc5dc0b4774c2e161c3bc8da217ed27345d780c
BRAIKI Ahmad <ahmedradhi.braiki@esprit.tn>
2018-07-16 11:23:25 +0100

    Fisrt commit

---
+++ SpringMVCHibernate/src/main/java/com/journaldev/spring/model/Person.java
@@ -0,0 +1,67 @@
+package com.journaldev.spring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity bean with JPA annotations
+ * Hibernate provides JPA implementation
+ * @author Ahmed
+ *
+ */
+@Entity
+@Table(name="PERSON")
+public class Person {
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+
+	private String contact;
+	
+	private String statut;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	
+	public String getStatut() {
+		return statut;
+	}
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+
+	@Override
+	public String toString(){
+		return "id="+id+", name="+name+", contact="+contact+", statut="+statut;
+	}
+}
